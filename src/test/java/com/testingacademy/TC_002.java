722b6ed0bf3355650835457a2548a2f26a48cba3
jay shankar <jayshankarprasad57@gmail.com>
2024-09-15 13:54:39 +0530

    restassured added

---
+++ src/test/java/com/testingacademy/TC_002.java
@@ -0,0 +1,25 @@
+package com.testingacademy;
+
+import io.restassured.RestAssured;
+
+public class TC_002 {
+    public static void main(String[] args) {
+        System.out.println("Rest Assured Test CASE");
+        System.out.println("GET Request Demo");
+
+        // Gherkins Syntax
+//        given() - url, headers, body or payload
+//        when() - http methods - get, post, patch, put, delete
+//        then() - verify the response - Expected Result == Actual Result
+        RestAssured
+                .given()
+                .baseUri("https://restful-booker.herokuapp.com")
+                .basePath("/booking/1").log().all()
+                .when()
+                .get()
+                .then().log().all()
+                .statusCode(200); // <201> but was <200>.
+
+
+    }
+}
